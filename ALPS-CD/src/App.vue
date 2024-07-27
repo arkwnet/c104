6ced0f7d8f163e6a284becba3819008835b358d3
Sora Arakawa <mail@arkw.net>
2024-07-27 14:54:32 +0900

    Create ALPS-CD

---
+++ ALPS-CD/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>ALPS-CD</div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {}
+  },
+  mounted() {},
+  computed: {}
+}
+</script>
---
+++ ALPS-CD/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import ScriptX from 'vue-scriptx'
+import './assets/main.css'
+
+createApp(App).use(ScriptX).mount('#app')
