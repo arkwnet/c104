f8a564cc30486d2a538aa0d1ec72f53f20416ffc
Sora Arakawa <mail@arkw.net>
2024-07-27 15:23:07 +0900

    ALPS-CD: Create background.png

--- ALPS-CD/src/App.vue
+++ ALPS-CD/src/App.vue
@@ -1,14 +1,39 @@
 <template>
-  <div>ALPS-CD</div>
+  <div>
+    <div class="photo"><img :src="imageUrl" /></div>
+    <canvas class="canvas" ref="canvas" width="1480" height="720"></canvas>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'App',
   data() {
-    return {}
+    return {
+      status: false,
+      image: new Image(),
+      imageUrl: '',
+      imageWidth: 1480,
+      imageHeight: 720,
+      canvas: null,
+      context: null
+    }
   },
-  mounted() {},
-  computed: {}
+  mounted() {
+    this.canvas = this.$refs.canvas
+    this.context = this.canvas.getContext('2d')
+    this.image.src = '/img/background.png'
+    this.update()
+  },
+  methods: {
+    update() {
+      this.context.drawImage(this.image, 0, 0, this.imageWidth, this.imageHeight)
+      this.imageUrl = this.canvas.toDataURL()
+      const vm = this
+      setTimeout(() => {
+        vm.update()
+      }, 500)
+    }
+  }
 }
 </script>
